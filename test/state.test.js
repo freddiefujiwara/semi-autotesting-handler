be2ac08059575bafd0560fb66b3b44e688bb4bb7
Fumikazu Fujiwara <github@ze.gs>
2017-12-29 07:18:47 +0900

    follow the rule of google

--- test/state.test.js
+++ test/state.test.js
@@ -37,35 +37,35 @@ describe('State test.', (suite) => {
         s.toString().should.be.a('string');
         const sf = new StateFactory('test/semi-autotesting-handler.sm');
         await sf.walk(await sf.load());
-        //initial
+        // initial
         sf.stateObjects['initial'].next('test_start')
             .should.have.property('name').with.equal('start');
 
-        //start
+        // start
         sf.stateObjects['start'].next()
             .should.have.property('name').with.equal('suite/initial');
 
-        //suite/initial
+        // suite/initial
         sf.stateObjects['suite/initial'].next()
             .should.have.property('name').with.equal('suite/case/initial');
 
-        //suite/case/initial
+        // suite/case/initial
         sf.stateObjects['suite/case/initial'].next()
             .should.have.property('name').with.equal('suite/case/given');
 
-        //suite/case/given
+        // suite/case/given
         sf.stateObjects['suite/case/given'].next('test_back')
             .should.have.property('name').with.equal('suite/case/given');
         sf.stateObjects['suite/case/given'].next('test_ok')
             .should.have.property('name').with.equal('suite/case/when');
 
-        //suite/case/when
+        // suite/case/when
         sf.stateObjects['suite/case/when'].next('test_back')
             .should.have.property('name').with.equal('suite/case/when');
         sf.stateObjects['suite/case/when'].next('test_ok')
             .should.have.property('name').with.equal('suite/case/then');
 
-        //suite/case/then
+        // suite/case/then
         sf.stateObjects['suite/case/then'].next('test_back')
             .should.have.property('name').with.equal('suite/case/then');
         sf.stateObjects['suite/case/then'].next('test_ok')
@@ -73,7 +73,7 @@ describe('State test.', (suite) => {
         sf.stateObjects['suite/case/then'].next('test_ng')
             .should.have.property('name').with.equal('suite/case/result');
 
-        //suite/case/result
+        // suite/case/result
         sf.stateObjects['suite/case/result'].next('test_finish')
             .should.have.property('name').with.equal('finish');
         sf.stateObjects['suite/case/result'].next('has_next')
@@ -81,17 +81,17 @@ describe('State test.', (suite) => {
         sf.stateObjects['suite/case/result'].next('does_not_have_next')
             .should.have.property('name').with.equal('suite/no_more_test');
 
-        //suite/no_more_test
+        // suite/no_more_test
         sf.stateObjects['suite/no_more_test'].next()
             .should.have.property('name').with.equal('finish');
 
-        //suite/has_next
+        // suite/has_next
         sf.stateObjects['suite/has_next'].next()
             .should.have.property('name').with.equal('suite/case/initial');
 
-        try{
+        try {
             sf.stateObjects['suite/case/result'].next();
-        }catch(e){
+        } catch (e) {
             e.should.be.a('error');
         }
     });
